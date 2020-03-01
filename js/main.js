f50a4bfb9316669b8ef3e9f70ebef6b567714552
Filippo Mailli <filippo.mailli@gmail.com>
2020-03-01 23:47:40 +0100

    .4 Funzioni jQuery e freccia-down sistemata

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
 
-    $('.freccia-down').click(function() {
-        
-            $(this).next('.dropdown-menu').toggle(500);
+    $('.menu-item').click(function() {
 
+        $('.menu-item').children('.freccia-down').removeClass('ruota');
+
+        if($(this).children('.dropdown-menu').is(':visible')) {
+            $(this).children('.dropdown-menu').slideToggle(200);
+            $('.menu-item').children('.freccia-down').removeClass('ruota');
+        } else {
+            $('.menu-item').children('.dropdown-menu').slideUp(200);
+            $(this).children('.dropdown-menu').toggle(200);
+            $(this).children('.freccia-down').addClass('ruota');
+        }
     });
 });
